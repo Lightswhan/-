35286290e251c2dc64baf8d32af110dc93edca78
Lightswhan <107972057+Lightswhan@users.noreply.github.com>
2022-06-24 16:20:02 +0800

    Add files via upload

---
+++ 升降晾衣杆/pages/start/start.js
@@ -0,0 +1,18 @@
+// start.js
+
+Page({
+    data: {
+
+    },
+    //跳转到天气页面
+    navigate: function() {
+        wx.navigateTo({
+            url: '../wifi_station/tianqi/tianqi',
+        })
+    },
+    navigate1: function() {
+        wx.navigateTo({
+            url: '../timer/timer',
+        })
+    }
+})
